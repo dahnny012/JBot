1f43614ba75530f23583fcfaa9ad56020e5a290f
dahnny012 <filler012@gmail.com>
2014-09-09 05:30:29 -0500

    main file

---
+++ hello.js
@@ -0,0 +1,56 @@
+var convoManager = [];
+
+function loadJQuery()
+{	var jNode = document.createElement("script");
+	jNode.src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+	document.body.appendChild(jNode);
+}
+
+function sayHello()
+{
+	// var temp
+	// get pointer 0
+// if(!getclassevent() != null)
+		// temp = event(Pointer)
+		// var name = getName()
+		// set message box html to (" ____ i am danh-bot")
+}
+
+function sendText(text)
+{
+
+}
+
+
+function main()
+{
+	loadJQuery();
+	// run a timeout function
+	if(waitForNoobs)
+	{
+		sayHello();
+	}
+	else
+	{
+		if(
+	}
+	//
+
+}
+
+
+
+function Person(name){
+	this.name = name;
+	this.chatPos = 0;
+	this.answered = false;
+}
+
+
+
+
+
+
+
+
+
