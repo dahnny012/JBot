cc5fdd70e6412997001c207de1c3d90522ec6899
Danh Nguyen <filler012@gmail.com>
2015-01-30 17:27:08 -0600

    Update mashiro.js
    This wont probably stay , its not dynamic enough

--- mashiro.js
+++ mashiro.js
@@ -1,8 +1,18 @@
 function Mashiro(){
+  
+  this.init =function(){
+    var name = this.addCategory("namae");
+    name.addDetails("Watashi wa Mashiro desu");
+    
+    var age = this.addCategory("sai");
+    age.addDetails("hatachi desu");
+    age.addDetails("tanjobi wa ....");
+  }
   this.aboutMe = {};
   
   this.addCategory = function(title){
     this.aboutMe.title = new Category();
+    return this.aboutMe.title;
   }
   this.searchKey = function(key){
     
