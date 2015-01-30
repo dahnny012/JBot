9b6f48dd9977116feb37abbcba18ab65c0eb8ae0
Danh Nguyen <filler012@gmail.com>
2015-01-30 17:21:59 -0600

    Update mashiro.js

--- mashiro.js
+++ mashiro.js
@@ -1,1 +1,24 @@
-function Person(){}
+function Mashiro(){
+  this.aboutMe = {};
+  
+  this.addCategory = function(title){
+    this.aboutMe.title = new Category();
+  }
+  this.searchKey = function(key){
+    
+  };
+}
+
+
+function Category(){
+  this.details = [];
+  this.addDetails = function(string){
+    this.details.push(string);
+  };
+  this.startDetail = function(string){
+    return this.details[0];
+  };
+  
+};
+
+
